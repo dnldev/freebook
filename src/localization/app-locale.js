e9c298247b05de099d83ccb856a9884f6d9faa59
unknown <daniel.ungersboeck@gmail.com>
2018-08-11 15:42:36 +0200

    add localization

---
+++ src/localization/app-locale.js
@@ -0,0 +1,14 @@
+import LocalizedStrings from 'react-localization';
+
+const strings = new LocalizedStrings({
+  en: {
+    search: 'Search',
+    send: 'Send',
+  },
+  de: {
+    search: 'Suchen',
+    send: 'Senden',
+  },
+});
+
+export default strings;
