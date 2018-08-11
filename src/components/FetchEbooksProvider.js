89b90d618cd5b187b829e4059fda8669f42a2645
unknown <daniel.ungersboeck@gmail.com>
2018-08-11 22:14:13 +0200

    remove sort add loading table adaptions add testdata

--- src/components/FetchEbooksProvider.js
+++ src/components/FetchEbooksProvider.js
@@ -1,40 +1,52 @@
 import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 
-import levenSort from 'leven-sort';
 import libgen from 'libgen';
 
 import { EbookProviderContext } from '../context';
 
+import testData from '../testdata';
+
 class FetchEbooksProvider extends PureComponent {
+  searchResultData = testData;
+
   state = {
+    columns: this.getColumnsArray('Title', 'Extension', 'Pages', 'Year'),
     inputs: [
       {
         fieldName: 'ebookName',
         fieldType: 'text',
+        initialValue: 'Clean Code',
       },
     ],
-    searchResults: [],
+    // searchResultTableData: [],
+    searchResultTableData: this.parseSearchResults(this.searchResultData),
+    searchLoading: false,
   };
+
+  // searchResultData = [];
 
   constructor(props) {
     super(props);
 
-    this.state.ebookChosen = this.ebookChosen.bind(this);
+    this.state.ebooksChosen = this.ebooksChosen.bind(this);
     this.state.fetchEbookList = this.fetchEbookList.bind(this);
   }
 
-  ebookChosen(chosenIndex) {
-    console.log(this.state.searchResults[chosenIndex]);
+  ebooksChosen(chosenIndexes) {
+    chosenIndexes.forEach(index => console.log(this.searchResultData[index]));
   }
 
   fetchEbookList(searchConfig) {
+    this.toggleSearchLoading();
+
     const { ebookName } = searchConfig;
 
     const options = {
       mirror: 'http://libgen.io',
       query: ebookName,
       search_in: 'title',
+      count: 20,
     };
 
     libgen.search(options, (err, data) => {
@@ -43,17 +55,49 @@ class FetchEbooksProvider extends PureComponent {
       }
 
       if (data) {
-        const sortedData = levenSort(data, ebookName, 'title');
-        this.setState({
-          searchResults: this.parseSearchResults(sortedData),
-        });
+        this.searchResultData = data;
+        console.log(this.searchResultData);
+        this.setState(
+          {
+            searchResultTableData: this.parseSearchResults(data),
+          },
+          () => this.toggleSearchLoading()
+        );
       }
     });
   }
 
-  parseSearchResults(data) {
-    return data.map(entry => `${entry.title} (${entry.extension})`);
+  getColumnsArray(...columns) {
+    return columns.map(column => {
+      if (typeof column === 'object') return column;
+
+      const columnTitle = column;
+      const columnKey = column.toLowerCase();
+
+      return {
+        title: columnTitle,
+        dataIndex: columnKey,
+        key: columnKey,
+      };
+    });
   }
+
+  parseSearchResults(data) {
+    return data.map(({ title, year, extension, pages }, index) => ({
+      key: index,
+      title,
+      extension,
+      pages,
+      year,
+    }));
+  }
+
+  toggleSearchLoading() {
+    this.toggleStateBool('searchLoading');
+  }
+
+  toggleStateBool = key =>
+    this.setState(prevState => ({ [key]: !prevState[key] }));
 
   render() {
     return (
