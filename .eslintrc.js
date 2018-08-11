385c9e94936995f4fbedd2941dcfa7ebe4f50647
unknown <daniel.ungersboeck@gmail.com>
2018-08-11 15:27:33 +0200

    update eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,16 @@
 module.exports = {
+  parser: 'babel-eslint',
   env: {
     browser: true,
     es6: true,
   },
   extends: ['eslint:recommended', 'plugin:react/recommended'],
   parserOptions: {
-    ecmaFeatures: {
-      arrowFunctions: true,
-      experimentalObjectRestSpread: true,
-      jsx: true,
-    },
     sourceType: 'module',
   },
   plugins: ['react'],
   rules: {
     indent: ['error', 2],
-    'linebreak-style': ['error', 'unix'],
     'no-console': 1,
     quotes: ['error', 'single'],
     semi: ['error', 'always'],
