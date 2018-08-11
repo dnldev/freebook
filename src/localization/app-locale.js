021fd04ce8c4415e287883b4b1128447e140fa12
unknown <daniel.ungersboeck@gmail.com>
2018-08-11 22:13:06 +0200

    update locale

--- src/localization/app-locale.js
+++ src/localization/app-locale.js
@@ -3,11 +3,25 @@ import LocalizedStrings from 'react-localization';
 const strings = new LocalizedStrings({
   en: {
     search: 'Search',
+    select: 'Select',
     send: 'Send',
+
+    table: {
+      filterConfirm: 'Ok',
+      filterReset: 'Reset',
+      emptyText: 'No Data',
+    },
   },
   de: {
     search: 'Suchen',
+    select: 'Auswählen',
     send: 'Senden',
+
+    table: {
+      filterConfirm: 'Ok',
+      filterReset: 'Zurücksetzen',
+      emptyText: 'Keine Daten',
+    },
   },
 });
 
