1fd82011ddb6f47af81d8b883f644e93c493d99f
unknown <daniel.ungersboeck@gmail.com>
2018-08-12 12:40:55 +0200

    add bytesInMbs function

--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,10 @@
+function bytesInMBs(bytes) {
+  return (bytes / 1000000).toFixed(2);
+}
+
 function camelToTitleCase(string) {
   const result = string.replace(/([A-Z])/g, ' $1');
   return result.charAt(0).toUpperCase() + result.slice(1);
 }
 
-module.exports = { camelToTitleCase }; // eslint-disable-line no-undef
+module.exports = { bytesInMBs, camelToTitleCase }; // eslint-disable-line no-undef
