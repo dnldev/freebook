559d70246b3cd94141d7a638f7ba93bf84a9bb73
unknown <daniel.ungersboeck@gmail.com>
2018-08-12 12:41:32 +0200

    add layout and styling use new SearchBar component instead of config

--- src/components/AppContent.js
+++ src/components/AppContent.js
@@ -1,20 +1,44 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import { Layout } from 'antd';
+
+import SearchBar from './SearchBar';
 import TableItemChooser from './TableItemChooser';
-import Config from './Config';
+
+import strings from '../localization/app-locale';
+
+const { Header, Content } = Layout;
+
+const navbarHeight = 64;
+const standardMargin = 8;
+
+const styles = {
+  content: {
+    height: `calc(100% - ${navbarHeight}px - ${standardMargin * 2}px)`,
+    margin: standardMargin,
+  },
+};
 
 const AppContent = ({ context }) => {
   return (
-    <React.Fragment>
-      <Config inputs={context.inputs} send={context.fetchEbookList} />
-      <TableItemChooser
-        columns={context.columns}
-        chooseItems={context.ebooksChosen}
-        items={context.searchResultTableData}
-        loading={context.searchLoading}
-      />
-    </React.Fragment>
+    <Layout>
+      <Header style={{ backgroundColor: 'white' }}>
+        {strings.headerTitle}
+      </Header>
+      <Content style={styles.content}>
+        <SearchBar name={strings.ebookName} send={context.fetchEbookList} />
+        <TableItemChooser
+          buttonText={strings.select}
+          columns={context.columns}
+          chooseItems={context.ebooksChosen}
+          expandedRowRender={context.expandedRowRender}
+          items={context.searchResultTableData}
+          loading={context.searchLoading}
+          tableLocale={strings.table}
+        />
+      </Content>
+    </Layout>
   );
 };
 
